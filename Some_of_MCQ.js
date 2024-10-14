dea64a4c4b00f661391dfe2e412959ad091aaf02
SudheerVelpula04 <velpulasudheer20@gmail.com>
2024-10-14 20:35:43 +0530

    mcqs

--- Some_of_MCQ.js
+++ Some_of_MCQ.js
@@ -13,4 +13,9 @@ const obj = Object.freeze({
   }
   
   console.log(obj.name, obj.info.age); // Output the final values
-  
+
+
+  // this is the explanation:
+  // The info property (which is an object itself) is not frozen by Object.freeze(). 
+  // Object.freeze() makes the top-level properties immutable, but nested objects can still be modified unless you explicitly freeze them.
+  // Therefore, obj.info.age = 30; successfully changes the age property from 25 to 30.
