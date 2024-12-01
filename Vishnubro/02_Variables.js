3303955494b612d95fd8455f36156f187ee554c3
SudheerVelpula04 <velpulasudheer0@gmail.com>
2024-12-01 21:45:22 +0530

    vishnubro

---
+++ Vishnubro/02_Variables.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS Code as newer version
+
+// alert(3+3)// We are using nodejs,not browser
+console.log("Vishnu");
+console.log(3 + 3); //Code readibility should be High
+/*Documentation : MDN Docs but its official documentation url: https://developer.mozilla.org/en-US/docs/Web/JavaScript
+Official Documentation tc39.com It defines EcmaScript Standards url:https://tc39.es/ecma262/#sec-intro */
+
+let name = "Vishnu";
+let age = 31;
+let isLoggedIn = false;
+
+//Number => 2 pow 53
+//bigInt
+//String => ""
+//Boolean =>true/false
+//Null => Standalone value
+let stat = null;
+//Undefined =>
+//Symbol ==> We will use it React component UNIQUENESS ,Figma tools Ho gaye
+
+//Object- 5-6 videos
+console.log(typeof stat); //This is the reason for many many errors
+console.log(typeof undefined);
